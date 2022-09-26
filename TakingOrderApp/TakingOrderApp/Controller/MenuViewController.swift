4ea8ecfb0248e2c9740525280770edf1d86a9a8a
Meric Keskin <mericyk@gmail.com>
2022-09-26 05:03:49 +0300

    Created table view cells for menu, made table view expandable for menu

--- TakingOrderApp/TakingOrderApp/Controller/MenuViewController.swift
+++ TakingOrderApp/TakingOrderApp/Controller/MenuViewController.swift
@@ -6,15 +6,169 @@
 //
 
 import UIKit
+import CoreData
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var menuTableView: UITableView!
+    
+    let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+    
+    var sections = [Section]()
+    var items = [MenuItem]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        menuTableView.register(UINib(nibName: "SectionTableViewCell", bundle: nil), forCellReuseIdentifier: "sectionCell")
+        menuTableView.register(UINib(nibName: "ItemTableViewCell", bundle: nil), forCellReuseIdentifier: "itemCell")
         
+        getSections()
+        if sections.isEmpty {
+            createSection(name: "Breakfast")
+            createSection(name: "Wraps")
+            createSection(name: "Burgers")
+            createSection(name: "Pizzas")
+            createSection(name: "Cold Drinks")
+            createSection(name: "Hot Drinks")
+
+            createItem(name: "Turkish Breakfast", section: "Breakfast", contents: ["egg", "cheese", "olive", "butter", "honey", "bread"])
+            createItem(name: "British Breakfast", section: "Breakfast", contents: ["egg", "ham", "tomato", "olive", "marmelade", "bun"])
+            createItem(name: "Veal Wrap", section: "Wraps", contents: ["veal meat", "cheese", "pickle", "bread"])
+            createItem(name: "Chicken Wrap", section: "Wraps", contents: ["chicken meat", "fries", "mustard", "bread"])
+            createItem(name: "Cheeseburger", section: "Burgers", contents: ["veal meat", "cheese", "pickle", "tomato", "mayonese"])
+            createItem(name: "BBQ Burger", section: "Burgers", contents: ["veal meat", "bacon", "pickle", "tomato", "BBQ"])
+            createItem(name: "Margherita", section: "Pizzas", contents: ["tomato sauce", "mozzarella"])
+            createItem(name: "Peperonni", section: "Pizzas", contents: ["tomato sauce", "mozzarella", "peperonni"])
+            createItem(name: "Coca Cola", section: "Cold Drinks", contents: [])
+            createItem(name: "Sprite", section: "Cold Drinks", contents: [])
+            createItem(name: "Tea", section: "Hot Drinks", contents: ["lemon"])
+            createItem(name: "Coffee", section: "Hot Drinks", contents: ["milk"])
+        }
+        
+//        deleteAllSections()
+//        deleteAllItems()
+        
+        getSections()
+        getItems()
     }
-
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if sections[section].isOpen {
+            return items.filter({ $0.section == sections[section].name }).count + 1
+        } else {
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            if let cell = menuTableView.dequeueReusableCell(withIdentifier: "sectionCell", for: indexPath) as? SectionTableViewCell {
+                cell.nameLbl.text = sections[indexPath.section].name
+                return cell
+            }
+        } else {
+            if let cell = menuTableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath) as? ItemTableViewCell {
+                cell.nameLbl.text = items.filter({ $0.section == sections[indexPath.section].name })[indexPath.row - 1].name
+                return cell
+            }
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        menuTableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0 {
+            sections[indexPath.section].isOpen = !sections[indexPath.section].isOpen
+            menuTableView.reloadSections([indexPath.section], with: .none)
+        } else {
+            let item = self.items.filter({ $0.section == sections[indexPath.section].name })[indexPath.row - 1]
+            let sheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            sheet.addAction(UIAlertAction(title: "Add Order", style: .default, handler: { [weak self] _ in
+                
+            }))
+            sheet.addAction(UIAlertAction(title: "Quick Add", style: .default, handler: { [weak self] _ in
+                
+            }))
+            sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            present(sheet, animated: true)
+        }
+    }
+    
+    func getSections() {
+        do {
+            sections = try context?.fetch(Section.fetchRequest()) ?? []
+            DispatchQueue.main.async {
+                self.menuTableView.reloadData()
+            }
+        } catch let e {
+            print(e)
+        }
+    }
+    
+    func createSection(name: String) {
+        if let context = context ?? self.context {
+            let newSection = Section(context: context)
+            newSection.name = name
+            newSection.isOpen = false
+        }
+        do {
+            try context?.save()
+            getSections()
+        } catch let e {
+            print(e)
+        }
+    }
+    
+    func getItems() {
+        do {
+            items = try context?.fetch(MenuItem.fetchRequest()) ?? []
+            DispatchQueue.main.async {
+                self.menuTableView.reloadData()
+            }
+        } catch let e {
+            print(e)
+        }
+    }
+    
+    func createItem(name: String, section: String, contents: [String]) {
+        if let context = context ?? self.context {
+            let newItem = MenuItem(context: context)
+            newItem.name = name
+            newItem.section = section
+            newItem.contents = contents
+        }
+        do {
+            try context?.save()
+            getItems()
+        } catch let e {
+            print(e)
+        }
+    }
+    
+//    func deleteAllSections() {
+//        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Section")
+//        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+//
+//        do {
+//            try context?.execute(deleteRequest)
+//        } catch let error as NSError {
+//            // TODO: handle the error
+//        }
+//    }
+//
+//    func deleteAllItems() {
+//        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "MenuItem")
+//        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+//
+//        do {
+//            try context?.execute(deleteRequest)
+//        } catch let error as NSError {
+//            // TODO: handle the error
+//        }
+//    }
 }
-
